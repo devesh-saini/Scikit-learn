1674ef54a313be53505533f7c69be96fe4374286
Devesh Saini <deveshsaini369@gmail.com>
2025-11-12 23:47:09 +0530

    initialized

---
+++ gettingStarted.py
@@ -0,0 +1,10 @@
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plot 
+
+Dataset = pd.read_csv("./startup_funding.csv")
+
+# print(Dataset.head())
+# print(Dataset.columns)
+
+
